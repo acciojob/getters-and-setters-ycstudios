3bd9ad74250d9f30d9fdb606c72c34ef34be380b
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-11 12:13:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,22 +3,23 @@ class Person {
         this._name = name;
         this._age = age;
     }
+
     get name() {
         return this._name;
     }
+
     set age(age) {
         this._age = age;
     }
 }
 
 class Student extends Person {
-
     study() {
         console.log(`${this.name} is studying`);
     }
 }
-class Teacher extends Person {
 
+class Teacher extends Person {
     teach() {
         console.log(`${this.name} is teaching`);
     }
