6fcb8ddebb219829e3a8c609b5c9f09c71808bde
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-11 09:33:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 class Person {
     constructor(name, age) {
-        this._name = name;
-        this._age = age;
+        this.name = name;
+        this.age = age;
     }
 
     get name() {
-        return this._name;
+        return this.name;
     }
 
     set age(age) {
-        this._age = age;
+        this.age = age;
     }
 }
 
