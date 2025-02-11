cc0263049bf0b8af809a16313927693776a674e3
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-11 12:12:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,30 +3,21 @@ class Person {
         this._name = name;
         this._age = age;
     }
-
     get name() {
         return this._name;
     }
-
     set age(age) {
         this._age = age;
     }
 }
 
 class Student extends Person {
-    constructor(name, age) {
-        super(name, age);
-    }
 
     study() {
         console.log(`${this.name} is studying`);
     }
 }
-
 class Teacher extends Person {
-    constructor(name, age) {
-        super(name, age);
-    }
 
     teach() {
         console.log(`${this.name} is teaching`);
